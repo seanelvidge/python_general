78fc4158ec94d0f77d7bfef4da1b93da9192791a
Sean Elvidge <seanelvidge@users.noreply.github.com>
2017-11-30 14:27:40 +0000

    Update wcDraw.py
    FIFA have released, in detail, how the World Cup group stage draw will be done. Code updated to reflect this.

--- wcDraw.py
+++ wcDraw.py
@@ -8,29 +8,36 @@ Related webpage: http://seanelvidge.com/2017/11/probabilityWorldCupGroup
 
 Created November 2017
 
+(Many thanks for comments provided by Julien Guyon)
+
 @author: sean@seanelvidge.com
 """
 # In case you're on Python2.7 (I am)
 from __future__ import print_function
 import numpy as np
 import datetime as dt
+from collections import Counter
 time = dt.datetime.now()
 
 # How many draw simulations?
-sims = 100000 # ~70s
+sims = 100000    # 10,000 = ~20s
 
 # Which team are you interested in?
 pickedTeam = "England"
 
 # Define some objects to hold the team and group information
-class team():
+class Team():
     def __init__(self, name, pot, fed):
         self.name = name
         self.pot = pot
         self.fed = fed
+        # This will tell us what group the team is in
+        self.selected = ''
+        # This tells us how many times this team was picked with our team of 
+        # interest throughout the simulations
         self.picked = 0
     
-class group():
+class Group():
     def __init__(self, gName):
         self.name = gName
         self.team1 = ''
@@ -38,44 +45,48 @@ class group():
         self.team3 = ''
         self.team4 = ''
         # These are max number of allowed confederation members per group
-        self.feds = dict(conmebol=1,concacaf=1,afc=1,uefa=2,caf=1)
+        self.feds = Counter(conmebol=1,concacaf=1,afc=1,uefa=2,caf=1)
+        
 
 # Define all teams, speciying name, pot number and confederation
-rus = team("Russia",1,"uefa")
-ger = team("Germany",1,"uefa")
-bra = team("Brazil",1,"conmebol")
-por = team("Portugal",1,"uefa")
-arg = team("Argentina",1,"conmebol")
-bel = team("Belgium",1,"uefa")
-pol = team("Poland",1,"uefa")
-fra = team("France",1,"uefa")
-spa = team("Spain",2,"uefa")
-per = team("Peru",2,"conmebol")
-swi = team("Switzerland",2,"uefa")
-eng = team("England",2,"uefa")
-col = team("Colombia",2,"conmebol")
-mex = team("Mexico",2,"concacaf")
-uru = team("Uruguay",2,"conmebol")
-cro = team("Croatia",2,"uefa")
-den = team("Denmark",3,"uefa")
-ice = team("Iceland",3,"uefa")
-cos = team("Costa Rica",3,"concacaf")
-swe = team("Sweden",3,"uefa")
-tun = team("Tunisa",3,"caf")
-egy = team("Egypt",3,"caf")
-sen = team("Senegal",3,"caf")
-ira = team("Iran",3,"afc")
-ser = team("Serbia",4,"uefa")
-nig = team("Nigeria",4,"caf")
-aus = team("Australia",4,"afc")
-jap = team("Japan",4,"afc")
-mor = team("Morocco",4,"caf")
-pan = team("Panama",4,"concacaf")
-kor = team("Korea Republic",4,"afc")
-sau = team("Saudi Arabia",4,"afc") 
+rus = Team("Russia",1,"uefa")
+ger = Team("Germany",1,"uefa")
+bra = Team("Brazil",1,"conmebol")
+por = Team("Portugal",1,"uefa")
+arg = Team("Argentina",1,"conmebol")
+bel = Team("Belgium",1,"uefa")
+pol = Team("Poland",1,"uefa")
+fra = Team("France",1,"uefa")
+spa = Team("Spain",2,"uefa")
+per = Team("Peru",2,"conmebol")
+swi = Team("Switzerland",2,"uefa")
+eng = Team("England",2,"uefa")
+col = Team("Colombia",2,"conmebol")
+mex = Team("Mexico",2,"concacaf")
+uru = Team("Uruguay",2,"conmebol")
+cro = Team("Croatia",2,"uefa")
+den = Team("Denmark",3,"uefa")
+ice = Team("Iceland",3,"uefa")
+cos = Team("Costa Rica",3,"concacaf")
+swe = Team("Sweden",3,"uefa")
+tun = Team("Tunisa",3,"caf")
+egy = Team("Egypt",3,"caf")
+sen = Team("Senegal",3,"caf")
+ira = Team("Iran",3,"afc")
+ser = Team("Serbia",4,"uefa")
+nig = Team("Nigeria",4,"caf")
+aus = Team("Australia",4,"afc")
+jap = Team("Japan",4,"afc")
+mor = Team("Morocco",4,"caf")
+pan = Team("Panama",4,"concacaf")
+kor = Team("Korea Republic",4,"afc")
+sau = Team("Saudi Arabia",4,"afc") 
 
 teams = [rus,ger,bra,por,arg,bel,pol,fra,spa,per,swi,eng,col,mex,uru,cro,den,
          ice,cos,swe,tun,egy,sen,ira,ser,nig,aus,jap,mor,pan,kor,sau]
+
+# Obvious but needed:
+groupOrder = ['A','B','C','D','E','F','G','H']
 
 # Create pots and numbers from each Confederation
 uefa = 0
@@ -106,6 +117,9 @@ for team in teams:
         pot3.append(team)
     if team.pot == 4:
         pot4.append(team)
+    # And the team we are interested in:
+    if team.name == pickedTeam:
+        pickTeam = team
 
 # Check numbers, should have (13+1) UEFA, 5 CAF, 5 AFC, 5 CONMEBOL, 3 CONCACAF
 # and 8 in each pot
@@ -114,51 +128,55 @@ if (uefa != 14) or (caf != 5) or (afc != 5) or (conmebol != 5) or (concacaf != 3
 if (len(pot1) != 8) or (len(pot2) != 8) or (len(pot3) != 8) or (len(pot4) != 8):
     raise ValueError('Pot numbers have gone wrong')
 
-# At the end we want to work out what group 'our' team is in, so we track it
-# through the draw process
-pickedGroup = -1
-
 # A similar structure is called multiple times in the loop, 
 # so we use a function:
-def addToGroups(groups, pot, team, pickedGroup):
-    # Sometimes we get draws which can't happen, so loop around until 
-    # every group is 'full'
-    full = False
-    while full != True:
-        randomPot = np.random.choice(pot,size=8,replace=False)
-        for n in np.arange(8):
-            # The number of groups which each team is allowed to go into is no 
-            # longer necessarily 8. Loop over groups:
-            allowedGroupInd = []
-            for g in np.arange(8):
-                if ((groups[g].feds[randomPot[n].fed] > 0) and 
-                    (getattr(groups[g],team) == '')):
-                    allowedGroupInd.append(g)
-            
-            if len(allowedGroupInd) != 0:
-                # Now pick a random one that is allowed
-                randInd = np.random.choice(allowedGroupInd)
-                if randomPot[n].name == pickedTeam:
-                    pickedGroup = randInd
-                setattr(groups[randInd], team, randomPot[n])
-                # Update the number of feds in the group
-                groups[randInd].feds[randomPot[n].fed] -= 1
-            
-        # Check that all groups have a team in:
-        full = True
-        for g in groups:
-            if getattr(g,team) == '': 
-                full=False
-        
-        if full == False:
-            # A group isn't full, so reset the groups for this team pick
-            for g in groups: 
-                if getattr(g,team) != '':
-                    # Update confeds info and set this team pick back to blank
-                    g.feds[getattr(g,team).fed] += 1
-                    setattr(g,team,'')
-            
-    return groups, pickedGroup
+def addToGroups(groups, pot, team):
+    randomPot = np.random.choice(pot,size=8,replace=False)
+    for pick in randomPot:
+        # Team should go in the next available group in alphabetical order.
+        # However we have to keep an eye on "the future" to make sure we don't
+        # enter an impossible position
+        for group in groups:
+            if ((group.feds[pick.fed] > 0) and 
+                (getattr(group,team) == '')):
+                # Team can go here. We don't 'formally' select the group yet
+                # we need to check what would happen if we did
+                setattr(group, team, pick)
+                group.feds[pick.fed] -= 1
+                pick.selected = group.name
+                # Will it make things impossible in the future?
+                # We check this by looking at remaining spaces and teams left
+                # to be picked:
+                spaces = Counter(conmebol=0,concacaf=0,afc=0,uefa=0,caf=0)
+                teamsToPick = Counter(conmebol=0,concacaf=0,afc=0,uefa=0,
+                                      caf=0)
+                # This slows down everything because we are going to run
+                # through every team in every group
+                for tmpG in groups:
+                    if getattr(tmpG, team) == '':
+                        confeds = tmpG.feds.copy()
+                        if confeds['uefa'] == 2:
+                            confeds['uefa'] -= 1
+                        spaces = spaces + confeds
+                # Now we have to see what teams are still left to be picked
+                for t in randomPot:
+                    if getattr(t, 'selected') == '':
+                        teamsToPick[getattr(t, 'fed')] += 1
+                # Subtract these two dictionaries from each other. If there are
+                # any negative values then this selection would be impossible
+                remainingDict = spaces.copy()
+                remainingDict.subtract(teamsToPick)
+                if (np.array(remainingDict.values()) < 0).any():
+                    # Then this wasn't a valid pick. So undo pick and try the
+                    # next group
+                    setattr(group, team, '')
+                    group.feds[pick.fed] += 1
+                    pick.selected = ''
+                else:
+                    # Break out of this for loop and move to next pick
+                    break
+                
+    return groups
             
 
 # Run through the simulations
@@ -167,49 +185,67 @@ for drawNum in np.arange(sims):
     # CONCACAF are allowed
 
     # 8 groups
-    groupA,groupB,groupC,groupD=group('A'),group('B'),group('C'),group('D')
-    groupE,groupF,groupG,groupH=group('E'),group('F'),group('G'),group('H')
+    groupA,groupB,groupC,groupD=Group('A'),Group('B'),Group('C'),Group('D')
+    groupE,groupF,groupG,groupH=Group('E'),Group('F'),Group('G'),Group('H')
     groups = [groupA,groupB,groupC,groupD,groupE,groupF,groupG,groupH]
     
     # Team A1 has to be Russia
     groups[0].team1 = rus
-    if 'Russia' == pickedTeam:
-        pickedGroup = 0
+    groups[0].feds['uefa'] -= 1
+    teams[0].selected = groups[0].name
     
     # Shrink pot1 to exclude Russia. Russia should be first team in pot1, but
     # add a quick error check just in case
     if pot1[0].name != 'Russia': raise ValueError()
     
-    # Draw rest of pot 1 teams into a random group
+    # Draw rest of pot 1 teams and place them in groups (in order)
     # Can't put someone else in Group A yet
     randomPot1 = np.random.choice(pot1[1:],size=7,replace=False)
-    randomGroupInd = np.random.choice(np.arange(1,8),size=7,replace=False)
-    
-    for n in np.arange(7):
+        
+    for n,group in enumerate(groups[1:]):
         if randomPot1[n].name == pickedTeam:
-            pickedGroup = randomGroupInd[n]
-        groups[randomGroupInd[n]].team1 = randomPot1[n]
-        groups[randomGroupInd[n]].feds[randomPot1[n].fed] -= 1
+            pickedGroup = n+1
+        group.team1 = randomPot1[n]
+        group.feds[randomPot1[n].fed] -= 1
+        randomPot1[n].selected = group
 
     # Now pot 2
-    groups, pickedGroup = addToGroups(groups, pot2, 'team2', pickedGroup)
+    groups = addToGroups(groups, pot2, 'team2')
 
     # Now pot 3
-    groups, pickedGroup = addToGroups(groups, pot3, 'team3', pickedGroup)
-    
+    groups = addToGroups(groups, pot3, 'team3')
+        
     # Now pot 4
-    groups, pickedGroup = addToGroups(groups, pot4, 'team4', pickedGroup)
-    
-    # The team we are interested in is in 'pickedGroup'. 
-    # Who else is with them?
-    for team in ['team1','team2','team3','team4']:
-        getattr(groups[pickedGroup],team).picked += 1
+    groups = addToGroups(groups, pot4, 'team4')
+
+    # Still can get impossible situations (~1% of the time). These arise from 
+    # the way I have counted possibilites (by adding together 'spaces'). 
+    # Exclude those cases:
+    fail = False
+    for t in teams:
+        if t.selected == '':
+            fail = True
+            
+    if fail == False:
+        # Which group is our interested team in?
+        intGroup = groups[np.where(np.array(groupOrder) == pickTeam.selected)[0][0]]
+        # Who else is with them?
+        for team in ['team1','team2','team3','team4']:
+            getattr(intGroup,team).picked += 1
+    else:
+        sims -= 1
+        
+    # Clean the team selected flag
+    for t in teams:
+        t.selected = ''
 
 
 # Finally print out the results
 for n,pot in enumerate([pot1,pot2,pot3,pot4]):
     print('Pot',n+1,':')
     for team in pot:
-        print('   ',team.name,':',team.picked*100./sims,'%')
+        print('   ',team.name,':',np.round(team.picked*100./sims,decimals=2),
+              '%')
         
+print(sims)
 print(dt.datetime.now()-time)
